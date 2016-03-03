eca059d26b6f03edfb05e8df3aaab7c362518352
Adina Howe <adina.chuang@gmail.com>
2016-03-03 07:54:45 -0600

    practice files

---
+++ phyloseq-tetM.R
@@ -0,0 +1,36 @@
+library(phyloseq)
+library(plyr)
+setwd('~/scratch/R-files/')
+all <- readRDS('tetM.RDS')
+
+#teasers
+plot_bar(all, "Environment")
+plot_richness(all, x='Environment')
+
+#http://joey711.github.io/phyloseq-demo/phyloseq-demo.html
+#Basic interactions
+all
+ntaxa(all)
+head(otu_table(all))
+sample_names(all)[1:10]
+sample_variables(all)
+rank_names(all)
+
+#subsetting
+topNOTUs = names(sort(taxa_sums(all), TRUE)[1:10])
+all_top10 = prune_taxa(topNOTUs, all)
+all_top10
+plot_bar(all_top10,fill="taxa",facet_grid = ~Environment)
+network_alltop10 = make_network(all_top10, "taxa", "jaccard", 0.3)
+plot_network(network_alltop10, all, "taxa", color = "taxa")
+
+
+#all sorts of tutorials
+#http://joey711.github.io/phyloseq/tutorials-index
+topNOTUs = names(sort(taxa_sums(all), TRUE)[1:100])
+all_top100 = prune_taxa(topNOTUs, all)
+all_top100
+plot_heatmap(all_top100)
+
+#A useful one 
+#http://joey711.github.io/phyloseq/preprocess.html
